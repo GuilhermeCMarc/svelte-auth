6c26b73c872842b82f6675a142edb408efc75e70
Guilherme Marcondes <guilherme.marcondes@tecnico.ulisboa.pt>
2022-10-06 20:35:29 +0100

    Added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="grid min-h-screen place-items-center px-6">
+	<div class="text-center">
+		<h1 class="mb-2 text-xl font-bold text-white">Error {$page.status}</h1>
+		<p>{$page.error?.message}</p>
+	</div>
+</div>
